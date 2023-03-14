80defdfdb4c8d9e62596705f444668fa2cf5deef
humayra akter <humayraakter25@gmail.com>
2023-03-14 23:14:45 +0600

    banner fancy

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,50 +1,49 @@
 import React from 'react';
-import { Link, Outlet, Route, Routes } from 'react-router-dom';
-import bouquet from '../../assets/icons/img1.png';
 import img1 from '../../assets/icons/img1.png';
 import img2 from '../../assets/icons/img2.png';
 import img3 from '../../assets/icons/img3.png';
 import img4 from '../../assets/icons/img4.png';
-
+import img5 from '../../assets/icons/img5 .png';
+import img6 from '../../assets/icons/img6.png';
+import img7 from '../../assets/icons/img7.png';
 
 const Banner = () => {
-
         return (
                 <div className='hero bg-pink-100 min-h-screen'>
-                        <div>
-                                <div className="card flex-col gap-16 px-10 lg:card-side">
-                                        <div className='flex'>
-                                                <Routes>
-                                                        <Route index ></Route>
-                                                </Routes>
-
-                                        </div>
-                                        <div className="drawer drawer-mobile">
-                                                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
-                                                <div className="drawer-content">
-                                                        <h1 className='p-10'><img className='px-6 py-px' src={bouquet} alt="Album" /></h1>
-                                                        <Outlet></Outlet>
+                        <div className="card flex-col gap-16 px-10 lg:card-side">
+                                <div>
+                                        <div class="h-96 carousel carousel-vertical rounded-box">
+                                                <div class="carousel-item h-full">
+                                                        <img src={img1} />
                                                 </div>
-                                                <div className="drawer h-80 mt-20 pl-0">
-                                                        <label for="banner-drawer" className="drawer-overlay"></label>
-                                                        <ul className="menu pl-0 p-4 flex w-20 bg-pink-100 text-sm font-bold text-pink-700 text-base-content">
-                                                                <li className='cursor-pointer w-16 hover:-translate-y-3' ><img src={img1} alt="Album" /></li>
-                                                                <li className='cursor-pointer w-16 hover:-translate-y-3' ><img src={img2} alt="Album" /></li>
-                                                                <li className='cursor-pointer w-16 hover:-translate-y-3' ><img src={img3} alt="Album" /></li>
-                                                                <li className='cursor-pointer w-16 hover:-translate-y-3' ><img src={img4} alt="Album" /></li>
-                                                        </ul>
-
+                                                <div class="carousel-item h-full">
+                                                        <img src={img2} />
+                                                </div>
+                                                <div class="carousel-item h-full">
+                                                        <img src={img3} />
+                                                </div>
+                                                <div class="carousel-item h-full">
+                                                        <img src={img4} />
+                                                </div>
+                                                <div class="carousel-item h-full">
+                                                        <img src={img5} />
+                                                </div>
+                                                <div class="carousel-item h-full">
+                                                        <img src={img6} />
+                                                </div>
+                                                <div class="carousel-item h-full">
+                                                        <img src={img7} />
                                                 </div>
                                         </div>
-                                        <div className="card-body">
-                                                <h2 className="text-5xl font-bold text-pink-700">When You Think Of Flowers</h2>
-                                                <h2 className="text-5xl font-bold text-pink-700">Think Of Ours</h2>
-                                                <p className='text-xs pt-6 font-semibold'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam eos illum molestiae recusandae quia, sequi odio qui vel maxime officiis.</p>
-                                                <button className='btn w-1/2 font-bold text-lg border-accent border-2 uppercase text-pink-700 bg-gradient-to-l from-accent to-secondary'>Get started</button>
-                                        </div>
+                                        <p className='text-pink-700 font-semibold text-center text-xs mt-7'><small>scroll down or scroll up</small></p>
+                                </div>
+                                <div className="card-body">
+                                        <h2 className="text-5xl font-bold text-pink-700">When You Think Of Flowers</h2>
+                                        <h2 className="text-5xl font-bold text-pink-700">Think Of Ours</h2>
+                                        <p className='text-xs pt-6 font-semibold'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam eos illum molestiae recusandae quia, sequi odio qui vel maxime officiis.</p>
+                                        <button className='btn w-1/2 font-bold text-lg border-accent border-2 uppercase text-pink-700 bg-gradient-to-l from-accent to-secondary'>Get started</button>
                                 </div>
                         </div>
-
                 </div>
         );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,11 +5,27 @@ import ContactDemo from './ContactDemo';
 import Info from './Info';
 import SellingExperience from './SellingExperience';
 import Services from './Services';
+import img1 from '../../assets/icons/img1.png';
+import img2 from '../../assets/icons/img2.png';
+import img3 from '../../assets/icons/img3.png';
+import img4 from '../../assets/icons/img4.png';
+import img5 from '../../assets/icons/img5 .png';
+import img6 from '../../assets/icons/img6.png';
+import img7 from '../../assets/icons/img7.png';
 
 const Home = () => {
         return (
                 <div className='bg-pink-100'>
                         <Banner></Banner>
+                        <div className='flex justify-center screen'>
+                                <img className='w-16 h-16 cursor-pointer hover:-translate-y-6 p-3' src={img1} alt="" />
+                                <img className='w-16 h-16 cursor-pointer hover:-translate-y-6 p-3' src={img2} alt="" />
+                                <img className='w-16 h-16 cursor-pointer hover:-translate-y-6 p-3' src={img3} alt="" />
+                                <img className='w-16 h-16 cursor-pointer hover:-translate-y-6 p-3' src={img4} alt="" />
+                                <img className='w-16 h-16 cursor-pointer hover:-translate-y-6 p-3' src={img5} alt="" />
+                                <img className='w-16 h-16 cursor-pointer hover:-translate-y-6 p-3' src={img6} alt="" />
+                                <img className='w-16 h-16 cursor-pointer hover:-translate-y-6 p-3' src={img7} alt="" />
+                        </div>
                         <Info></Info>
                         <Services></Services>
                         <Bouquet></Bouquet>

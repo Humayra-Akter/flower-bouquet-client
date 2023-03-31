2bcb0ee8ea1c5252e2442f1ecd6750a75379187a
humayra akter <humayraakter25@gmail.com>
2023-03-31 13:40:12 +0600

    contact

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import icon1 from '../../assets/images/location.png';
+import icon2 from '../../assets/images/email.png';
+import icon3 from '../../assets/images/questions.png';
 
 const Contact = () => {
         return (
-                <div>
-                        
-                </div>
+                <section className='mt-20 px-4'>
+                        <div className='flex gap-5 p-20'>
+                                <div class="card w-96 bg-base-100 shadow-xl">
+                                        <figure><img className='w-16 h-16' src={icon1} /></figure>
+                                        <div class="card-body">
+                                                <h2 className="text-center text-xl font-bold text-pink-700">Our Location</h2>
+                                                <p className='text-center'> (800) 123 456 789 / (800) 123 456 789</p>
+                                                <p className='text-center'> info@example.com</p>
+                                        </div>
+                                </div><div class="card w-96 bg-base-100 shadow-xl">
+                                        <figure><img className='w-16 h-16' src={icon2} /></figure>
+                                        <div class="card-body">
+                                                <h2 className="text-center text-xl font-bold text-pink-700">Contact us Anytime</h2>
+                                                <p className='text-center'>Mobile: 012 345 678</p>
+                                                <p className='text-center'>Fax: 123 456 789</p>
+                                        </div>
+                                </div><div class="card w-96 bg-base-100 shadow-xl">
+                                        <figure><img className='w-16 h-16' src={icon3} /></figure>
+                                        <div class="card-body">
+                                                <h2 className="text-center text-xl font-bold text-pink-700">Support Overall</h2>
+                                                <p className='text-center'>Support24/7@example.com</p>
+                                                <p className='text-center'>info@example.com</p>
+                                        </div>
+                                </div>
+                        </div>
+                        <div className="w-full p-16">
+                                <h1 className='text-center text-2xl font-bold text-pink-700'>Contact Us</h1>
+                                <div className="card-body">
+                                        <div className='flex gap-4'>
+                                                <div className="form-control w-1/2">
+                                                        <input type="text" placeholder="name" className="input input-bordered border-pink-700" />
+                                                </div>
+                                                <div className="form-control w-1/2">
+                                                        <input type="text" placeholder="email" className="input input-bordered border-pink-700" />
+                                                </div>
+                                        </div>
+                                        <div className="form-control">
+                                                <input type="text" placeholder="subject" className="input input-bordered border-pink-700" />
+                                        </div>
+                                        <div className="form-control">
+                                                <input type="text" placeholder="your message" className="input input-lg input-bordered border-pink-700" />
+                                        </div>
+                                        <div className="form-control mt-6">
+                                                <button className="btn font-bold text-lg border-secondary border-2 uppercase text-pink-700 bg-gradient-to-l from-accent to-secondary">GET in touch</button>
+                                        </div>
+                                </div>
+                        </div>
+                </section>
         );
 };
 
--- src/Pages/Home/ContactDemo.js
+++ src/Pages/Home/ContactDemo.js
@@ -1,42 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import bg from '../../assets/images/bg.png'
+import bg from '../../assets/images/purple-bg.png'
 
 const ContactDemo = () => {
         return (
-                <section className='mt-56 px-4'>
-
-                        <div
-                                style={{
-                                        background: `url(${bg})`,
-                                        backgroundSize: 'cover'
-                                }} className="hero min-h-screen">
-                                <div className="card w-full max-w-lg">
-                                        <h1 className='text-center text-2xl font-bold text-pink-700'>Contact Us</h1>
-                                        <div className="card-body">
-                                                <div className="form-control">
-                                                        <label className="label">
-                                                                <span className="label-text">Email</span>
-                                                        </label>
-                                                        <input type="text" placeholder="email" className="input input-bordered" />
-                                                </div>
-                                                <div className="form-control">
-                                                        <label className="label">
-                                                                <span className="label-text">Password</span>
-                                                        </label>
-                                                        <input type="text" placeholder="password" className="input input-bordered" />
-                                                        <label className="label">
-                                                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                                        </label>
-                                                </div>
-                                                <div className="form-control mt-6">
-                                                        <button className="btn font-bold text-lg border-secondary border-2 uppercase text-pink-700 bg-gradient-to-l from-accent to-secondary">Login</button>
-                                                </div>
-
-                                                <div className="divider text-xl font-bold text-pink-900">OR</div>
-                                                <div className="form-control mt-6">
-                                                        <button
-                                                                className="btn btn-md font-bold text-lg border-secondary border-2 uppercase text-pink-700 bg-gradient-to-l from-accent to-secondary"><Link className='text-pink-700 font-bold' to="/login">Login to account</Link></button>
+                <section className='mt-14'>
+                        <div style={{
+                                background: `url(${bg})`,
+                                backgroundSize: 'cover'
+                        }} className='flex m-2'>
+                                <div className='hero flex-1 pl-10'>
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/XCA3igbFg3I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                </div>
+                                <div className="hero min-h-screen">
+                                        <div className="card w-full max-w-lg">
+                                                <h1 className='text-center text-2xl font-bold text-pink-700'>Contact Us</h1>
+                                                <div className="card-body">
+                                                        <div className="form-control">
+                                                                <input type="text" placeholder="email" className="input input-bordered" />
+                                                        </div>
+                                                        <div className="form-control">
+                                                                <input type="text" placeholder="your message" className="input-lg input-bordered" />
+                                                        </div>
+                                                        <div className="form-control mt-6">
+                                                                <button className="btn font-bold text-lg border-secondary border-2 uppercase text-pink-700 bg-gradient-to-l from-accent to-secondary">Submit</button>
+                                                        </div>
                                                 </div>
                                         </div>
                                 </div>
